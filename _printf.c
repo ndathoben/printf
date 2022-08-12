a12e2e973c6913542f3786d37aaec81e6d554046
ndathoben <ndathob@gmail.com>
2022-08-12 12:24:42 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,159 +1,239 @@
 #include "main.h"
-/* check for nulls in strings */
-#define NULL_S(S) ((S) == NULL ? (-1) : (1))
+#define INT_BITS (4 * 8)
+#define STDOUT 1
 
+int subc(char *new_pointer, va_list list, int buff_cou);
+int subs(char *new_pointer, va_list list, int buff_cou);
+int subi(char *new_pointer, va_list list, int buff_cou);
+int subb(char *new_pointer, va_list list, int buff_cou);
+int subr(char *new_pointer, va_list list, int buff_cou);
+int subR13(char *new_pointer, va_list list, int buff_cou);
 /**
- * print_args - print given variadic arguments given type specifier from printf
- * @args: all the ... arguments
- * @format:the current suspect specifier
+ * _printf - prints a string like output according to a format.
+ * @format: Is a character string, it is composed of zero or more directives.
  *
- * Return: no of chars printed or error
- * Description:
- * j will be used to loop through the real specifiers for each of the
- * suspect specifiers to know if they are real.
- * if they are real, we'll use the switch statement to print the ... var
- * correctly.
- * if they are not, we can print an error message, or just skip it,
- * depending on which stage this program is on
- */
-int print_args(const char format, va_list args)
-{
-	char *s;
-	int count, i = 0;
-	bool format_in_array = false;
-	char specifiers[8] = {'c', 'i', 'd', 'o', 'b', 'x', 'X', 'u'};
-	int (*print_func_arr[])(int) = {
-	print_char, print_int, print_decimal, print_octal, print_binary,
-	print_hexadecimal_small, print_hexadecimal_caps, print_unsigned
-	};
-
-	for (; i < 8; i++)
-	{
-		if (format == specifiers[i])
-		{
-			format_in_array = true;
-			break;
-		}
-	}
-	if (format_in_array == true)
-		count = (*print_func_arr[i])(va_arg(args, int));
-	else
-	{
-		switch (format)
-		{
-		case 's':
-			s = va_arg(args, char *);
-			if (NULL_S(s) == -1)
-				return (-1);
-			count = print_string(s);
-			break;
-		case '%':
-			count = print_char('%');
-			break;
-		default:
-			count = -1;
-			break;
-		}
-	}
-	return (count);
-}
-
-/**
- * check_null - check if the _printf function has
- * a string that does not act as we expect
- * this has nothing to do with our variadic
- * @format: the string we wanna check
- * Return: 0 if ok else -1
- */
-int check_null(const char *format)
-{
-	char *temp;
-	int i, j, r_val = 1;
-	char options[] = {'c', 's', 'd', 'i', '%', '\0'};
-
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-	{
-		return (-1);
-	}
-
-	/* check for % without correct specifiers */
-	i = 0;
-	temp = (char *)format;
-	while (i < _strlen(temp))
-	{
-		j = 0;
-		while (j < 5)
-		{
-			if ((format[i] == '%' &&
-			     format[i + 1] == options[j] &&
-			     (i + 1) < _strlen(temp)))
-			{
-				r_val = 0;
-			}
-			if (format[i] == '%' &&
-			    (i + 1) >= _strlen(temp))
-			{
-				r_val = -1;
-				break;
-			}
-
-			j++;
-		}
-		i++;
-	}
-	/* check if there was a mismatch with our flags */
-	if (r_val < 0)
-	{
-		return (-1);
-	}
-	return (0);
-}
-
-/**
- * _printf - prints anything, clone for printf()
- * @format:string to be printed
- * if string is null, prints nil
- *
- * Return: no of chars printed or error
+ * Description: _printf writes the output to stdout.
+ * Return: The number of characters printed, avoiding the end null byte.
  */
 int _printf(const char *format, ...)
 {
-	int i, count, isnull;
-	char *temp;
-	va_list args;
+	int counter = 0, buff_cou = 0;
+	char new_pointer[2048];
+	va_list list;
 
-	isnull = check_null(format);
-	if (isnull < 0)
-	{
+	va_start(list, format);
+
+	if (format == NULL)
 		return (-1);
-	}
-
-	va_start(args, format);
-
-	temp = (char *)format; /* just avoiding some errors */
-	if (_strcmp(temp, "") == 0)
+	for (counter = 0; format != NULL && format[counter] != '\0'; counter++)
 	{
-		print_string("(nil)");
-		return (-1);
-	}
-
-	count = 0;
-	i = 0;
-	while (i < _strlen(temp))
-	{
-		if (format[i] == '%')
+		if (format[counter] == '%')
 		{
-			/* hoping that args will sort everything out */
-			count += print_args(format[i + 1], args);
-			i = i + 2;
+			counter++;
+			switch (format[counter])
+			{
+				case 's':
+					buff_cou = subs(new_pointer, list, buff_cou);
+					break;
+				case 'c':
+					buff_cou = subc(new_pointer, list, buff_cou);
+					break;
+				case 'i': case 'd':
+					buff_cou = subi(new_pointer, list, buff_cou);
+					break;
+				case 'b':
+					buff_cou = subb(new_pointer, list, buff_cou);
+					break;
+				case 'r':
+					buff_cou = subr(new_pointer, list, buff_cou);
+					break;
+				case 'R':
+					buff_cou = subR13(new_pointer, list, buff_cou);
+					break;
+				case '%':
+					new_pointer[buff_cou] = '%', buff_cou++;
+					break;
+				case '\0':
+					return (-1);
+				default:
+					counter--;
+					new_pointer[buff_cou] = '%';
+					new_pointer[buff_cou] = format[counter], buff_cou++;
+			}
 		}
 		else
 		{
-			print_char(format[i]);
-			count++;
-			i++;
+			new_pointer[buff_cou] = format[counter], buff_cou++;
 		}
 	}
-	va_end(args);
-	return (count);
+	new_pointer[buff_cou] = '\0';		/* setting end character */
+	write(STDOUT, new_pointer, buff_cou);/* salida del buffer */
+	va_end(list);
+	return (buff_cou);
+}
+/**
+ * subi - substitute %i by argument number
+ * @new_pointer: string to change
+ * @list: va_list char to change
+ * @buff_cou: index of dst where the c of %c is
+ * Return: New index
+ */
+int subi(char *new_pointer, va_list list, int buff_cou)
+{
+	int tens = 1;
+	unsigned int tmp;
+	int number;
+
+	number = va_arg(list, int);
+
+	if (number < 0)
+	{
+		new_pointer[buff_cou] = '-';
+		buff_cou++;
+		number = -number;
+	}
+	tmp = number;
+	/* caso especial number = INT_MIN */
+	if (number == INT_MIN)
+	{
+		tmp++;
+	}
+	/* convertir tens en el 10^n maximo divisible entre number*/
+	while (tmp > 9)
+	{
+		tens = tens * 10;
+		tmp = tmp / 10;
+	}
+
+	tmp = number;
+	while (tens > 0)
+	{
+		new_pointer[buff_cou] = ('0' + tmp / tens);
+		buff_cou++;
+		tmp %= tens;
+		tens /= 10;
+	}
+	return (buff_cou);
+}
+/**
+ * subc - substitute %c by the list element
+ * @new_pointer: string to change
+ * @list: va_list char to change
+ * @buff_cou: index of dst where the c of %c is
+ * Return: New index
+ */
+int subc(char *new_pointer, va_list list, int buff_cou)
+{
+	int z_c;
+
+	z_c = va_arg(list, int);
+	new_pointer[buff_cou] = z_c;
+	buff_cou++;
+	return (buff_cou);
+}
+/**
+ * subs - prints a character.
+ * @new_pointer: Is a character string,
+ * it is composed of zero or more directives.
+ * @list: string list of arguments.
+ * @buff_cou: index of dst where the c of %c is
+ * Description: _subs writes a character.
+ * Return: The number of characters printed, avoiding the end null byte.
+ */
+int subs(char *new_pointer, va_list list, int buff_cou)
+{
+	char *value;
+	int i;
+
+	value = va_arg(list, char *);
+	if (value == NULL)
+		value = "(null)";
+	for (i = 0; value[i]; i++, buff_cou++)
+		new_pointer[buff_cou] = value[i];
+	return (buff_cou);
+}
+/**
+ * subb - substitute %b by argument number in binary
+ * @new_pointer: string to change
+ * @list: va_list char to change
+ * @buff_cou: index of dst where the c of %c is
+ * Return: New index
+ */
+int subb(char *new_pointer, va_list list, int buff_cou)
+{
+	unsigned int bit, number;
+	int MSB = 0;
+	int i;
+
+	number = va_arg(list, unsigned int);
+	for (i = INT_BITS; i > 0; i--)
+	{
+		bit = number >> (i - 1); /* delete rigth bits of bit */
+		bit = bit & 1; /* delete left bits of bit */
+		MSB = MSB | bit;
+		if (MSB != 0 || i == 1) /* i == 1 to show b0 */
+		{
+			new_pointer[buff_cou] = ('0' + bit);
+			buff_cou++;
+		}
+	}
+	return (buff_cou);
+}
+/**
+ * subr - substitute %r by argument number in binary
+ * @new_pointer: string to change
+ * @list: va_list char to change
+ * @buff_cou: index of dst where the c of %c is
+ * Return: New index
+ */
+int subr(char *new_pointer, va_list list, int buff_cou)
+{
+	int i = 0;
+	char *sr;
+
+	sr = va_arg(list, char*);
+	if (sr)
+	{
+		while (sr[i] != '\0')
+			i++;
+		i = i - 1;
+		while (i >= 0)
+		{
+			new_pointer[buff_cou] = sr[i];
+			buff_cou++;
+			i--;
+		}
+	}
+	return (buff_cou);
+}
+/**
+ * subR13 - substitute %r by argument number in binary
+ * @new_pointer: string to change
+ * @list: va_list char to change
+ * @buff_cou: index of dst where the c of %c is
+ * Return: New index
+ */
+int subR13(char *new_pointer, va_list list, int buff_cou)
+{
+	char *str;
+	char *keysrc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *keydst = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int key_i, carry = 0;
+
+	str = va_arg(list, char*);
+	while (str[carry])
+	{
+		for (key_i = 0; keysrc[key_i]; key_i++)
+		{
+			if (str[carry] == keysrc[key_i])
+			{
+				new_pointer[buff_cou] = keydst[key_i];
+				break;
+			}
+			new_pointer[buff_cou] = str[carry];
+		}
+		buff_cou++;
+		carry++;
+	}
+	return (buff_cou);
 }
